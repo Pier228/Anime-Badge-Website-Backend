1b6da108cf4544100d0a5dbb1aa97cbca439b483
Pier228 <artem.buxanenko@gmail.com>
2024-04-16 11:07:05 +0300

    Added images preview

--- frontend/src/components/AnimeBadge.tsx
+++ frontend/src/components/AnimeBadge.tsx
@@ -2,11 +2,12 @@ import { IDanceFloor } from "../Interfaces/IAnimeBadge";
 
 const AnimeBadge = (props: IDanceFloor) => {
     return (
-        <div className="border-2 mx-auto border-solid border-indigo-600 w-9/12 p-4 overflow-auto min-h-48 relative flex flex-col gap-y-3">
+        <div className="border-2 mx-auto border-solid border-indigo-600 w-9/12 p-4 overflow-auto min-h-96 relative flex flex-col gap-y-3">
             <p>#{props.count}</p>
             <p>Name: {props.name}</p>
             <p className="whitespace-nowrap">Source: {props.src}</p>
             <p>ID: {props._id}</p>
+            <img src={props.src} alt={props.name} className="object-contain w-40 h-40"/>
         </div>
     );
 };
